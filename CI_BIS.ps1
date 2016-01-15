518f117d1ed3edd121b1957e3b4b60689a61d529
Hongxiong Xian (Wicresoft) <v-hoxian@microsoft.com>
2016-01-15 15:59:16 +0800

    Add the script to prepare xml for test

--- CI_BIS.ps1
+++ CI_BIS.ps1
@@ -18,4 +18,107 @@ Copy-Item CI\ssh\*   BIS\$os_on_host\lisa\ssh
 New-Item  -ItemType "Directory" BIS\$os_on_host\lisa\bin
 Copy-Item CI\tools\*   BIS\$os_on_host\lisa\bin
 
+
+# Update config for CI Run
+$XmlConfigFile = $env:XmlConfigFile
+if ($XmlConfigFile -and (Test-Path "$pwd\BIS\WS2012R2\lisa\xml\freebsd\$XmlConfigFile"))
+{
+	CIUpdateConfig "$pwd\BIS\WS2012R2\lisa\xml\freebsd\$XmlConfigFile" "$pwd\BIS\WS2008R2\lisa" run.xml 
+}
+else
+{
+	#TODO
+}
+
+
+
+
+
+<#
+Usage:
+	CIUpdateConfig $originalConfigFile $CIFolder $newConfigFileName $runfileName
+Description:
+	This is a function to update cloud configuration for CI job.
+#>
+# Function CIUpdateConfig([string]$originalConfigFile, [string]$CIFolder, [string]$newConfigFileName, [string]$runfileName)
+Function CIUpdateConfig([string]$originalConfigFile, [string]$CIFolder, [string]$newConfigFileName)
+{
+	# $runfile = "$CIFolder\$runfileName"
+	$newConfigFile = "$CIFolder\$newConfigFileName"
+    
+    # The $newConfigFileName is a copy of $originalConfigFile. All changes will be written into the $newConfigFileName
+    Copy-Item $originalConfigFile $newConfigFile
+
+
+	[xml]$xml = Get-Content "$newConfigFile"
+
+
+	# Update Distro
+	# $deploymentData = $xml.config.Azure.Deployment.Data
+	# $deploymentData.Distro[0].Name = $env:DistroName
+	# $deploymentData.Distro[0].OsImage = $env:DistroOsImage
+	# if($env:DistroOsVHD)
+	# {				
+		# if (!($deploymentData.Distro[0].OsVHD))
+		# {
+			# $newNode = $xml.CreateElement("OsVHD")
+			# $deploymentData.Distro[0].AppendChild($newNode)
+		# }
+		# $deploymentData.Distro[0].OsVHD = $env:DistroOsVHD
+	# }
+
+	
+	# Update the config XML file for Cloud Testing
+	# $UpdateEnvStatus = UpdateCICloudEnvironmentSettings $xml
+	# if ($UpdateEnvStatus -eq $False)
+	# {
+		# Write-Host "Error: UpdateCICloudEnvironmentSettings failed."
+		# exit
+	# }
+	
+	
+
+	
+	# For debugging cloud cases in CI
+	# if($env:DebugCases -and $env:DebugCases.Trim() -ne "")
+	# {
+		# $debugCycle = $xml.SelectSingleNode("/config/testCycles/Cycle[cycleName=`"DEBUG`"]")
+		# if($debugCycle)
+		# {
+			# foreach($testcase in $debugCycle.test)
+			# {
+				# $testcase = $debugCycle.RemoveChild($testcase)
+			# }
+		# }
+		# else
+		# {
+			# $debugCycle = $xml.CreateElement("Cycle")
+			# $name = $xml.CreateElement("cycleName")
+			# $name.InnerText = "DEBUG"
+			# $name = $debugCycle.AppendChild($name)
+			# $debugCycle = $xml.DocumentElement.testCycles.AppendChild($debugCycle)
+		# }
+		
+		# foreach($cn in ($env:DebugCases).Trim().Split(","))
+		# {
+			# $debugCase = $xml.CreateElement("test")
+			# $debugCaseName = $xml.CreateElement("Name")
+			# $debugCaseName.InnerText = $cn.Trim()
+			# $debugCaseName = $debugCase.AppendChild($debugCaseName)
+			# $debugCase = $debugCycle.AppendChild($debugCase)
+		# }
+	# }
+
+	$xml.Save("$newConfigFile")
+}
+
+
+
+
+
+
+
+
+
+
 "Prepare the xml for test done"

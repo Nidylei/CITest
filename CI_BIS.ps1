32dd35b6e6d576a515da2294b86124096406bb62
Hongxiong Xian (Wicresoft) <v-hoxian@microsoft.com>
2016-01-26 13:57:06 +0800

    Get ip

--- CI_BIS.ps1
+++ CI_BIS.ps1
@@ -1,8 +1,5 @@
 
 
-
-
-
 #######################################################################
 #
 # GetIPv4ViaKVP()
@@ -105,14 +102,12 @@ function GetIPv4([String] $vmName, [String] $server)
     .Example
         GetIPv4 $testVMName $serverName
     #>
-"Begin to get ipv4"
     $errMsg = $null
     $addr = GetIPv4ViaKVP $vmName $server
     if (-not $addr)
     {
-		#TODO?
+		#TODO
 		"Warning: Cannot get ipv4 from kvp."
-		"Add is $ $addr *********************"
     }
 	
     return $addr
@@ -156,7 +151,7 @@ function DoStartVM([String] $vmName, [String] $server)
     }
     else
     {
-		"Go to sleep 60 until the vm boot successfully"
+		"Go to sleep 60 to wait the vm boot successfully"
 		sleep 60
 		"Start vm $vmName successfully."
     }
@@ -166,29 +161,25 @@ function DoStartVM([String] $vmName, [String] $server)
 
 <#
 Usage:
-	CIUpdateConfig $originalConfigFile $CIFolder $newConfigFileName $runfileName
+	CIUpdateConfig $originalConfigFile $CIFolder $newConfigFileName
 Description:
 	This is a function to update cloud configuration for CI job.
 #>
-# Function CIUpdateConfig([string]$originalConfigFile, [string]$CIFolder, [string]$newConfigFileName, [string]$runfileName)
 Function CIUpdateConfig([string]$originalConfigFile, [string]$CIFolder, [string]$newConfigFileName)
 {
-
-	# $runfile = "$CIFolder\$runfileName"
 	$newConfigFile = "$CIFolder\$newConfigFileName"
     
     # The $newConfigFileName is a copy of $originalConfigFile. All changes will be written into the $newConfigFileName
     Copy-Item $originalConfigFile $newConfigFile
 
-
 	[xml]$xml = Get-Content "$newConfigFile"
 
 
-	
 	$vmName = $xml.config.VMs.vm.vmName
 	"The vm name is $vmName before change"
 	$TestSuite = $xml.config.VMs.vm.suite
 	"The test suite is $suite before change"
+	"--------------------------------------------------"
 	
 	# Update vmName
 	$xml.config.VMs.vm.vmName = $env:VMName
@@ -196,67 +187,67 @@ Function CIUpdateConfig([string]$originalConfigFile, [string]$CIFolder, [string]
 	# Update test suite
 	$xml.config.VMs.vm.suite = $env:TestSuite
 	
+	# Update test hvServer
 	$server = "localhost"
-	$ipv4_addr = GetIPv4 $env:VMName $server
+	$xml.config.VMs.vm.hvServer = $server
 	
-	# Update vmName
+	# Update ipv4
+	$ipv4_addr = GetIPv4 $env:VMName $server
 	$xml.config.VMs.vm.ipv4 = [string]$ipv4_addr
 
-	#GetIPv4  in utilFunctions.ps1
-	
-	# $deploymentData.Distro[0].Name = $env:DistroName
-	# $deploymentData.Distro[0].OsImage = $env:DistroOsImage
-	# if($env:DistroOsVHD)
-	# {				
-		# if (!($deploymentData.Distro[0].OsVHD))
-		# {
-			# $newNode = $xml.CreateElement("OsVHD")
-			# $deploymentData.Distro[0].AppendChild($newNode)
-		# }
-		# $deploymentData.Distro[0].OsVHD = $env:DistroOsVHD
-	# }
 
-	
-	# Update the config XML file for Cloud Testing
-	# $UpdateEnvStatus = UpdateCICloudEnvironmentSettings $xml
-	# if ($UpdateEnvStatus -eq $False)
-	# {
-		# Write-Host "Error: UpdateCICloudEnvironmentSettings failed."
-		# exit
-	# }
-	
-	
 
+
+	# "--------------------------just for test-------------------------------"
+	# $newConfigFile = "D:\CI\workspace\CI.BIS2012R2Test\BIS\WS2012R2\lisa\run.xml"
+	# [xml]$xml = Get-Content "D:\CI\workspace\CI.BIS2012R2Test\BIS\WS2012R2\lisa\run.xml"
 	
-	# For debugging cloud cases in CI
-	# if($env:DebugCases -and $env:DebugCases.Trim() -ne "")
-	# {
-		# $debugCycle = $xml.SelectSingleNode("/config/testCycles/Cycle[cycleName=`"DEBUG`"]")
-		# if($debugCycle)
-		# {
-			# foreach($testcase in $debugCycle.test)
-			# {
-				# $testcase = $debugCycle.RemoveChild($testcase)
-			# }
-		# }
-		# else
-		# {
-			# $debugCycle = $xml.CreateElement("Cycle")
-			# $name = $xml.CreateElement("cycleName")
-			# $name.InnerText = "DEBUG"
-			# $name = $debugCycle.AppendChild($name)
-			# $debugCycle = $xml.DocumentElement.testCycles.AppendChild($debugCycle)
-		# }
+	# if(1)
+	if($env:DebugCases -and $env:DebugCases.Trim() -ne "")
+	{
+		$debugCycle = $xml.SelectSingleNode("/config/testSuites/suite[suiteName=`"debug`"]")
+		if($debugCycle)
+		{
 		
-		# foreach($cn in ($env:DebugCases).Trim().Split(","))
-		# {
-			# $debugCase = $xml.CreateElement("test")
-			# $debugCaseName = $xml.CreateElement("Name")
-			# $debugCaseName.InnerText = $cn.Trim()
-			# $debugCaseName = $debugCase.AppendChild($debugCaseName)
-			# $debugCase = $debugCycle.AppendChild($debugCase)
-		# }
-	# }
+			foreach($testcase in $debugCycle.suiteTests.suiteTest)  #Just for test
+			{
+				"Test cases are $testcase"
+			}
+			
+			foreach($testcase in $debugCycle.suiteTests)
+			{
+				$testcase = $debugCycle.RemoveChild($testcase)
+			}
+			
+			# $RemoveList = $xml.config.testSuites.suite | Where-Object {$_.suiteName -eq "debug2"}
+			# $xml.config.testSuites.RemoveChild($RemoveList)
+
+		}
+		else
+		{
+		    # "Run here ....1..."
+			$debugCycle = $xml.CreateElement("suite")
+			$name = $xml.CreateElement("suiteName")
+			$name.InnerText = "DEBUGxhx"
+			$name = $debugCycle.AppendChild($name)
+			$debugCycle = $xml.DocumentElement.testSuites.AppendChild($debugCycle)
+		}
+		
+	
+		# $DebugCases = "testcase1,testcase2,testcase3,CheckMemoryCapacity-5GB,4K_AddDynamicVHDX_SCSIDrive_Multi"
+		# $DebugCases = "testcase1,testcase2,testcase3"
+		
+		$debugCase = $xml.CreateElement("suiteTests")
+		foreach($cn in ($DebugCases).Trim().Split(","))
+		{
+			 # "Run here ....2..."
+		
+			$debugCaseName = $xml.CreateElement("suiteTest")
+			$debugCaseName.InnerText = $cn.Trim()
+			$debugCaseName = $debugCase.AppendChild($debugCaseName)
+			$debugCase = $debugCycle.AppendChild($debugCase)
+		}
+	}
 
 	$xml.Save("$newConfigFile")
 }
@@ -267,12 +258,10 @@ Function CIUpdateConfig([string]$originalConfigFile, [string]$CIFolder, [string]
 
 "Prepare the xml for test"
 
-#TODO: WS2012R2? WS2008R2?
 $os_on_host = $env:HostOS
-$os_on_host = "WS2012R2"  #Just for test
+# $os_on_host = "WS2012R2"  #Just for test
 
 # Copy certificate
-
 New-Item  -ItemType "Directory" BIS\$os_on_host\lisa\ssh
 Copy-Item CI\ssh\*   BIS\$os_on_host\lisa\ssh
 
@@ -281,12 +270,12 @@ New-Item  -ItemType "Directory" BIS\$os_on_host\lisa\bin
 Copy-Item CI\tools\*   BIS\$os_on_host\lisa\bin
 
 
-"PWD is $pwd -------------------"
+"PWD is $pwd -------------------"   #Just for test
 
-$server = "localhost"
-$env:VMName="FreeBSD64xhx"  #Just for test
-DoStartVM $env:VMName $server
+# $env:VMName="FreeBSD64xhx"  #Just for test
+DoStartVM $env:VMName "localhost"
 
+#Just for test
 # $ipaddr=GetIPv4 $env:VMName $server
 # "IP is $ipaddr"
 # "******************************"
@@ -303,7 +292,7 @@ if ($XmlConfigFile -and (Test-Path "$pwd\BIS\$os_on_host\lisa\xml\freebsd\$XmlCo
 else
 {
 	#TODO
-	CIUpdateConfig "$pwd\BIS\$os_on_host\lisa\xml\freebsd\$XmlConfigFile" "$pwd\BIS\$os_on_host\lisa" run.xml   #Just for test
+	# CIUpdateConfig "$pwd\BIS\$os_on_host\lisa\xml\freebsd\$XmlConfigFile" "$pwd\BIS\$os_on_host\lisa" run.xml   #Just for test
 }
 
 
